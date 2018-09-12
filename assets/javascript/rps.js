b080c74b0739fa87baa3c33d0dabf5ad825eba80
Jesse W. Salmon <j_aw_s@icloud.com>
2018-09-11 23:44:22 -0700

    initial commit: layout of controls and chat

---
+++ assets/javascript/rps.js
@@ -0,0 +1,13 @@
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCmvxuM183nvcLbEBecAg8AOVstwnf880s",
+    authDomain: "jws-rps-multiplayer.firebaseapp.com",
+    databaseURL: "https://jws-rps-multiplayer.firebaseio.com",
+    projectId: "jws-rps-multiplayer",
+    storageBucket: "",
+    messagingSenderId: "871272250305"
+  };
+  firebase.initializeApp(config);
+</script>
